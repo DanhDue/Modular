2c4f178ddc1c232ccadc01b7f2048ee8ad328e93
DanhDue ExOICTIF <danhdue@gmail.com>
2022-08-10 14:11:29 +0700

    init the project.

---
+++ Modular/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Modular
+//
+//  Created by DanhDue ExOICTIF on 10/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Chat 💬").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
